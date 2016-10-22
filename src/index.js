8cfbd25ccbfc4e79f7f9007974ad71892679badd
Aliaksei Sapach <dreamsonic@tut.by>
2016-10-22 22:29:00 +0300

    added scaffolding

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export default function ({ types: t }) {
+  return {
+    visitor: {
+      Program: function () {
+
+      }
+    }
+  };
+}
---
+++ test/index.js
@@ -0,0 +1,30 @@
+import path from 'path';
+import fs from 'fs';
+import assert from 'assert';
+import { transformFileSync } from 'babel-core';
+import plugin from '../src';
+
+function trim(str) {
+  return str.replace(/^\s+|\s+$/, '');
+}
+
+describe('Plugin', () => {
+  const fixturesDir = path.join(__dirname, 'fixtures');
+  fs.readdirSync(fixturesDir).map((caseName) => {
+    it(`should ${caseName.split('-').join(' ')}`, () => {
+      const fixtureDir = path.join(fixturesDir, caseName);
+      const actualPath = path.join(fixtureDir, 'actual.js');
+      const options = {
+        babelrc: false,
+        plugins: [plugin]
+      };
+
+      const actual = transformFileSync(actualPath, options).code;
+      const expected = fs.readFileSync(
+        path.join(fixtureDir, 'expected.js')
+      ).toString();
+
+      assert.equal(trim(actual), trim(expected));
+    });
+  });
+});
