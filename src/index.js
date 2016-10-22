5fb3302a4ac2616bf8a59ba160923d29dc075b0c
Aliaksei Sapach <dreamsonic@tut.by>
2016-10-22 23:00:57 +0300

    added default export transform

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-export default function ({ types: t }) {
+export default function ({types: t}) {
   return {
     visitor: {
       Program: function () {
-
+      },
+      ExportDefaultDeclaration: function (path) {
+        if (t.isIdentifier(path.node.declaration)) {
+          path.replaceWith(t.ExportNamedDeclaration(null, [
+            t.exportSpecifier(path.node.declaration, t.identifier('default'))
+          ]));
+        }
       }
     }
   };
