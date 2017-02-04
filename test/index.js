6d06bd94857952d3fd7e06f82cb2f7393c5e98ad
Aliaksei Sapach <dreamsonic@tut.by>
2017-02-05 00:11:42 +0300

    refactored tests

--- test/index.js
+++ test/index.js
@@ -2,11 +2,8 @@ import path from 'path';
 import fs from 'fs';
 import assert from 'assert';
 import { transformFileSync } from 'babel-core';
+import { trim } from './util';
 import plugin from '../src';
-
-function trim(str) {
-  return str.replace(/^\s+|\s+$/, '');
-}
 
 describe('Plugin', () => {
   const fixturesDir = path.join(__dirname, 'fixtures');
