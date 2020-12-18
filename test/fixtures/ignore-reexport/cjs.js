ad55fbf231f48cb2816e73ce89a4efbe1e8d59b9
Aliaksei Sapach <dreamsonic@tut.by>
2020-12-18 18:40:22 +0300

    updated dependencies

--- test/fixtures/ignore-reexport/cjs.js
+++ test/fixtures/ignore-reexport/cjs.js
@@ -20,6 +20,7 @@ var _baz = require("./baz.js");
 Object.keys(_baz).forEach(function (key) {
   if (key === "default" || key === "__esModule") return;
   if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+  if (key in exports && exports[key] === _baz[key]) return;
   Object.defineProperty(exports, key, {
     enumerable: true,
     get: function get() {
--- test/fixtures/transform-multiple-exports/es5.js
+++ test/fixtures/transform-multiple-exports/es5.js
@@ -1,3 +1,5 @@
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 var foo = function foo() {
   return bar();
 },
@@ -16,8 +18,6 @@ var foo = function foo() {
     }
   }, bar);
 });
-
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
